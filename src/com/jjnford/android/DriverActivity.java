a18d26bda9ac949791ecb50efd52cf60d94c7f48
JJ Ford <jj.n.ford@gmail.com>
2012-04-07 16:04:05 -0500

    Remove setContentView() from Activity.

--- src/com/jjnford/android/DriverActivity.java
+++ src/com/jjnford/android/DriverActivity.java
@@ -1,7 +1,5 @@
 package com.jjnford.android;
 
-import com.jjnford.android.shell.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,7 +7,6 @@ public class DriverActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.onCreate(savedInstanceState);        
     }
 }
