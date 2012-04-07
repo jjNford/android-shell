50c3eb2deb864e8f487af71f708f8baff80df58b
JJ Ford <jj.n.ford@gmail.com>
2012-04-07 17:00:13 -0500

    Remote unused packages from Activity.

--- src/com/jjnford/android/DriverActivity.java
+++ src/com/jjnford/android/DriverActivity.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class DriverActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);        
+    	super.onCreate(savedInstanceState);
     }
 }
